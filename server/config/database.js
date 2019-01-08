3e657ba998996d4f5609e39bb7967a4318d5e14e
anthony.schwartz <antschw8@hotmail.com>
2019-01-08 13:34:20 +0100

    *- liaison avec la bdd showroom

--- server/config/database.js
+++ server/config/database.js
@@ -15,7 +15,7 @@ db = mysql.createConnection(
     {
             host     : 'localhost',
             port     : 3306,
-            user     : 'badint',
-            password : '123456',
+            user     : 'uhaSQL',
+            password : 'uha',
             database : 'showroom',
     });
