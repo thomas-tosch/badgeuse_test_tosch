4cdcd3191c78bbd3d936c966f3325ba230e90ee8
anthony.schwartz <antschw8@hotmail.com>
2019-01-11 12:38:55 +0100

    *- finition de la requète SQL

--- server/routes/middlewares/admin/graph.js
+++ server/routes/middlewares/admin/graph.js
@@ -19,7 +19,7 @@ module.exports = function(router) {
                     [startDate],
                     [endDate]
                 ];
-                db.query('SELECT CONCAT(users.nom_user, \' \', users.prenom_user) AS id_user, DATE_FORMAT(CAST(start_point AS DATE), "%d-%m-%Y") date_point, CAST(start_point as TIME) startTime, CAST(end_point as TIME) endTime, SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF( end_point, start_point )))) AS duration FROM badger INNER JOIN users ON badger.id_user = users.id_user WHERE end_point IS NOT NULL AND start_point BETWEEN ? AND ? GROUP BY id_user ORDER BY id_user ', content, (err, rows)=> {
+                db.query('SELECT CONCAT(users.nom_user, \' \', users.prenom_user) AS userName, DATE_FORMAT(CAST(start_point AS DATE), "%d-%m-%Y") date_point, CAST(start_point as TIME) startTime, CAST(end_point as TIME) endTime, SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF( end_point, start_point )))) AS duration FROM badger INNER JOIN users ON badger.id_user = users.id_user WHERE end_point IS NOT NULL AND start_point BETWEEN ? AND ? GROUP BY userName ORDER BY userName ', content, (err, rows)=> {
                     if(err) throw err;
 
                     res.json({message: rows});
