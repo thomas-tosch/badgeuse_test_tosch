8f90714987043c210085046ba324c549d62befeb
franck.hubschwerle <franck.hubschwerle@uha.fr>
2019-01-24 15:29:30 +0100

    form request sans mise en page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {ForgotPassComponent} from "./guest/forgot-pass/forgot-pass.component";
 import {NewPassComponent} from "./guest/new-pass/new-pass.component";
 import {GraphComponent} from "./admin/graph/graph.component";
 import {UserDetailComponent} from "./admin/user-detail/user-detail.component";
+import {FormRequestComponent} from "./user/form-request/form-request.component";
 
 // ROUTER
 const routes: Routes = [
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: 'userDetail/:id_user', component: UserDetailComponent, canActivate: [AuthGuard] },
 
   { path: 'userSpace', component: PersonalSpaceComponent, canActivate: [AuthGuard]},
+  { path: 'userRequest', component: FormRequestComponent , canActivate: [AuthGuard]},
 
   { path: 'login', component: LoginComponent, canActivate: [NotAuthGuard] },
   { path: 'newPass/:id_user/:key', component: NewPassComponent, canActivate: [NotAuthGuard] },
--- client/src/app/user/form-request/form-request.component.ts
+++ client/src/app/user/form-request/form-request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-form-request',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-request.component.css']
 })
 export class FormRequestComponent implements OnInit {
+
+  formRequest: FormGroup;
 
   constructor() { }
 
