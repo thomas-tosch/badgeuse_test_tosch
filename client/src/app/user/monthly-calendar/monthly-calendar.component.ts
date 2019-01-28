11afcf26a0473240bda159ba5a0a3ae95805888c
gauthier.staehler <gauthier.staehler@uha.fr>
2019-01-28 16:29:15 +0100

    Délimitation des horaires du calendrier hebdomadaire

--- client/src/app/user/monthly-calendar/monthly-calendar.component.ts
+++ client/src/app/user/monthly-calendar/monthly-calendar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CalendarComponent } from 'ng-fullcalendar';
 import { Options } from 'fullcalendar';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-monthly-calendar',
@@ -21,6 +22,9 @@ export class MonthlyCalendarComponent implements OnInit {
         eventLimit : false,
         slotLabelFormat : 'H(:mm)',
         allDaySlot : false,
+        minTime : moment.duration('07:00:00'),
+        maxTime : moment.duration('20:00:00'),
+        height : 730,
         header : {
             left : 'title',
             center : '',
