79c2b8a612ecd409a04a8260adda1ddcc6d89606
gauthier.staehler <gauthier.staehler@uha.fr>
2019-01-28 15:20:50 +0100

    Ajout du calendrier hebdomadaire

--- client/src/app/user/monthly-calendar/monthly-calendar.component.ts
+++ client/src/app/user/monthly-calendar/monthly-calendar.component.ts
@@ -8,24 +8,23 @@ import { Options } from 'fullcalendar';
   styleUrls: ['./monthly-calendar.component.css']
 })
 export class MonthlyCalendarComponent implements OnInit {
-
   calendarOptions: Options;
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
   constructor() {}
   ngOnInit() {
     this.calendarOptions = {
+        defaultView : 'agendaWeek',
         showNonCurrentDates : true,
         weekends : false,
         locale: 'fr',
         editable: true,
         eventLimit: false,
         header: {
-            left: 'prev',
-            center: 'title',
-            right: 'next'
+            left: 'title',
+            center: '',
+            right: 'prev, month, agendaWeek, next'
         },
       // events: data
     };
   }
-
 }
