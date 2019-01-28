bfc45c5ea5f7960c45fe2b7f423504d68355854b
gauthier.staehler <gauthier.staehler@uha.fr>
2019-01-28 11:56:56 +0100

    Nettoyage jQuery

--- client/src/app/user/monthly-calendar/monthly-calendar.component.ts
+++ client/src/app/user/monthly-calendar/monthly-calendar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CalendarComponent } from 'ng-fullcalendar';
 import { Options } from 'fullcalendar';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-monthly-calendar',
