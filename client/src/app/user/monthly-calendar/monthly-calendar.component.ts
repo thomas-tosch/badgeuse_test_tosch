f527dc40ae5765e7cff0c7b4198625d54e99d382
gauthier.staehler <gauthier.staehler@uha.fr>
2019-02-01 09:52:29 +0100

    Test sur backend

--- client/src/app/user/monthly-calendar/monthly-calendar.component.ts
+++ client/src/app/user/monthly-calendar/monthly-calendar.component.ts
@@ -1,55 +1,71 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Auth} from "../../guards/auth";
-import { CalendarService } from "../../services/calendar.service";
-import { CalendarComponent } from 'ng-fullcalendar';
-import { Options } from 'fullcalendar';
+import {CalendarService} from "../../services/calendar.service";
+import {CalendarComponent} from 'ng-fullcalendar';
+import {Options} from 'fullcalendar';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'app-monthly-calendar',
-  templateUrl: './monthly-calendar.component.html',
-  styleUrls: ['./monthly-calendar.component.css']
+    selector: 'app-monthly-calendar',
+    templateUrl: './monthly-calendar.component.html',
+    styleUrls: ['./monthly-calendar.component.css']
 })
 export class MonthlyCalendarComponent implements OnInit {
-  calendarOptions: Options;
-  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
-  constructor(private expressService: CalendarService) {}
-  ngOnInit() {
+    calendarOptions: Options;
+    @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
 
-    this.calendarOptions = {
-        defaultView : 'agendaWeek',
-        showNonCurrentDates : true,
-        weekends : false,
-        locale : 'fr',
-        editable : true,
-        eventLimit : false,
-        slotLabelFormat : 'H(:mm)',
-        allDaySlot : false,
-        minTime : moment.duration('07:00:00'),
-        maxTime : moment.duration('20:00:00'),
-        height : 730,
-        header : {
-            left : 'title',
-            center : '',
-            right : 'prev, agendaWeek, month, next'
-        },
-        buttonText : {
-            month:    'Mois',
-            week:     'Semaine'
-        },
-        events: [
-            {
-                start: '2019-02-01T08:25:16',
-                end: '2019-02-01T17:08:52',
-                rendering: 'background'
+    constructor(private expressService: CalendarService) {
+    }
+
+    ngOnInit() {
+
+        this.getBackend();
+
+        this.calendarOptions = {
+            defaultView: 'agendaWeek',
+            showNonCurrentDates: true,
+            weekends: false,
+            locale: 'fr',
+            editable: true,
+            eventLimit: false,
+            slotLabelFormat: 'H(:mm)',
+            allDaySlot: false,
+            minTime: moment.duration('07:00:00'),
+            maxTime: moment.duration('20:00:00'),
+            height: 730,
+            header: {
+                left: 'title',
+                center: '',
+                right: 'prev, agendaWeek, month, next'
             },
-            {
-                start: '2019-01-31',
-                end: '2019-01-31',
-                rendering: 'background',
-                color  : '#FF6347'
-            }
-        ],
-    };
-  }
+            buttonText: {
+                month: 'Mois',
+                week: 'Semaine'
+            },
+            events: [
+                {
+                    start: '2019-02-01T08:25:16',
+                    end: '2019-02-01T17:08:52',
+                    rendering: 'background'
+                },
+                {
+                    start: '2019-01-31',
+                    end: '2019-01-31',
+                    rendering: 'background',
+                    color: '#FF6347'
+                }
+            ],
+        };
+    }
+
+    getBackend() {
+
+        const content = {
+            absence: ''
+        };
+        this.expressService.postExpress('calendar', content).subscribe((res: Auth) => {
+            console.log(res.success);
+            console.log(res.message);
+        });
+    }
 }
--- server/routes/middlewares/user/calendar.js
+++ server/routes/middlewares/user/calendar.js
@@ -1,16 +1,16 @@
-require ('../../../config/database');
+require('../../../config/database');
 
-module.exports = function(router) {
+module.exports = function (router) {
 
     router.post('/', (req, res) => {
 
-        const action = req.body.action;
         let absence = req.body.absence;
 
-        db.query('SELECT * FROM absences', [absence], (err, rows)=> {
+        db.query('SELECT * FROM absences', [absence], (err, rows) => {
 
             res.json({
                 success: true,
+                message: absence
             });
         });
 
