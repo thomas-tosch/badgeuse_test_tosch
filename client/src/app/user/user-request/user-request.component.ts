8e824511acf938f17607055271dee6fcb78f7c5c
anthony.schwartz <antschw8@hotmail.com>
2019-02-05 13:52:18 +0100

    *- paufination du formulaire

--- client/src/app/user/user-request/user-request.component.ts
+++ client/src/app/user/user-request/user-request.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {LoginService} from "../../services/login.service";
 import {ExpressService} from "../../services/express.service";
 import {Router} from "@angular/router";
@@ -14,67 +14,81 @@ import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 })
 export class UserRequestComponent implements OnInit {
 
-  userRequest: FormGroup;
-  processing = false;
-  faInfoCircle = faInfoCircle
+    userRequest: FormGroup;
+    processing = false;
+    faInfoCircle = faInfoCircle;
+    justifiedPeriod = true;
 
-  constructor(private formBuilder: FormBuilder,)
-  {
+    constructor(private formBuilder: FormBuilder,)
+    {
     this.createForm();
-  }
+    }
 
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 
+    onJustifiedPeriod() {
+        this.justifiedPeriod = !this.justifiedPeriod;
+        if(!this.justifiedPeriod) {
+            this.userRequest.get('dateDebut').clearValidators();
+            this.userRequest.get('dateDebut').updateValueAndValidity();
+            this.userRequest.get('dateDebut').setValue(null);
+            this.userRequest.get('dateFin').clearValidators();
+            this.userRequest.get('dateFin').updateValueAndValidity();
+            this.userRequest.get('dateFin').setValue(null);
+            this.userRequest.get('inputDateOnly').setValidators([Validators.required]);
+            this.userRequest.get('inputDateOnly').updateValueAndValidity();
+        } else {
+            this.userRequest.get('dateDebut').setValidators([Validators.required]);
+            this.userRequest.get('dateDebut').updateValueAndValidity();
+            this.userRequest.get('dateFin').setValidators([Validators.required]);
+            this.userRequest.get('dateFin').updateValueAndValidity();
+            this.userRequest.get('inputDateOnly').clearValidators();
+            this.userRequest.get('inputDateOnly').updateValueAndValidity();
+            this.userRequest.get('inputDateOnly').setValue(null);
+        }
+    }
 
+    createForm() {
+        this.userRequest = this.formBuilder.group({
+            reason: ['', Validators.required],
+            dateDebut: [null, Validators.required],
+            dateFin: [null, Validators.required],
+            inputDateOnly: [null],
+            checkboxHalfDay: [null],
+            comment: ['']
+        });
+    }
 
-  createForm() {
-    this.userRequest = this.formBuilder.group({
-      Motif: ['', Validators.required],
-      DateDebut: ['', Validators.required],
-      DateFin: ['', Validators.required],
-      comment: ['', Validators.required]
+    disableForm() {
+        this.userRequest.controls['reason'].disable();
+        this.userRequest.controls['dateDebut'].disable();
+        this.userRequest.controls['dateFin'].disable();
+        this.userRequest.controls['comment'].disable();
 
-    });
-  }
+    }
 
-  disableForm() {
-    this.userRequest.controls['Motif'].disable();
-    this.userRequest.controls['DateDebut'].disable();
-    this.userRequest.controls['DateFin'].disable();
-    this.userRequest.controls['comment'].disable();
+    enableForm() {
+        this.userRequest.controls['reason'].enable();
+        this.userRequest.controls['dateDebut'].enable();
+        this.userRequest.controls['dateFin'].enable();
+        this.userRequest.controls['comment'].enable();
 
-  }
+    }
 
-  enableForm() {
-    this.userRequest.controls['Motif'].enable();
-    this.userRequest.controls['DateDebut'].enable();
-    this.userRequest.controls['DateFin'].enable();
-    this.userRequest.controls['comment'].enable();
+    onRequestSubmit() {
 
-  }
-
-   onRequestSubmit() {
-
-    //console.log(this.userRequest.get('Motif').value);
-   /* const toast = swal.mixin({
-      toast: true,
-      position: "top",
-      showConfirmButton: true;
-      timer: 3000
-    });
-
-    this.processing = true;
-    this.disableForm();
-*/
-
-    console.log(
-      this.userRequest.get('Motif').value,
-      this.userRequest.get('DateDebut').value,
-      this.userRequest.get('DateFin').value,
-      this.userRequest.get('comment').value)
+        let content = {
+            reason: this.userRequest.get('reason').value,
+            dateDebut: this.userRequest.get('dateDebut').value,
+            dateFin: this.userRequest.get('dateFin').value,
+            inputDateOnly: this.userRequest.get('inputDateOnly').value,
+            checkboxHalfDay: this.userRequest.get('checkboxHalfDay').value,
+            comment: this.userRequest.get('comment').value
+        };
+        console.log(content);
     };
 
     // TODO gerrer le pacourrir justificatif, vérifier onValidation différence avec onLoginSubmit (BDD?) mettre type inputDate
