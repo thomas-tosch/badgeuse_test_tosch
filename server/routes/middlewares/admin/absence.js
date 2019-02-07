4c2c8241434ddca4f87ecc7e9bd5a3a394089a01
chloe.prudham <chloe.prudham@uha.fr>
2019-02-07 13:41:08 +0100

    Absence: affichage sur le tableau des noms, demi-journee et raison. Button Valide/Refuse.

--- server/routes/middlewares/admin/absence.js
+++ server/routes/middlewares/admin/absence.js
@@ -18,10 +18,23 @@ module.exports = function(router) {
 
                ];
 
-                 db.query('SELECT * FROM absences ' +
+                 db.query('SELECT ' +
+                     'CONCAT(users.nom_user, \' \', users.prenom_user) AS absName, ' +
                      '' +
-                     'INNER JOIN users ON absences.id_user = users.id_user ' +
+                     'reason.nom_reason AS absReason, ' +
+                     '' +
+                     'CASE WHEN half_day < 1 THEN "Non" ELSE "Oui" END AS miAbs' +
+                     '' +
+                     ' FROM ((absences ' +
+                     '' +
+                     'INNER JOIN users ON absences.id_user = users.id_user) ' +
+                     'INNER JOIN reason ON absences.id_reason = reason.id_reason) ' +
+                     //'LEFT JOIN (SELECT id_user, ref_absence, id_status, absence_date, id_reason FROM absences WHERE id_status = 2 AND GROUP BY ref_absence) absences ON users.id_user = absences.id_user ' + // join users table table with absence table. Select all between date and summe de absence day
                      'WHERE id_status = 2'
+                     //'' +
+                     //'GROUP BY red_absence ' +
+                     //'' +
+                     //'ORDER BY ??, absName' // select order by
                      , content, (err, rows) => {
                    console.log(rows);
                         if(err) {
