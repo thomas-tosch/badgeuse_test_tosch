9f659f1edb46ed3555fc51b08f29357f168e5301
gauthier.staehler <gauthier.staehler@uha.fr>
2019-02-08 11:30:22 +0100

    Correction orthographique

--- client/src/app/guest/login/login.component.ts
+++ client/src/app/guest/login/login.component.ts
@@ -93,7 +93,7 @@ export class LoginComponent implements OnInit {
         // the modal success
         toast({
           type: 'success',
-          title: 'Authentification réussi !'
+          title: 'Authentification réussie !'
         })
 
         // redirection
