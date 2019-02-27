70b178255afbb0db762878d7ab93d6aa87be099a
schwartz <anthony.schwartz@uha.fr>
2019-02-27 16:13:38 +0100

    *- rendu et compilation final pour la prod

--- client/src/app/pipe/replaceSpace.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'replaceSpace'})
-export class ReplaceSpace implements PipeTransform {
-
-    transform(value: string): string {
-        return value.replace(' ', '_');
-    }
-}
--- server/config/config.js
+++ server/config/config.js
@@ -23,24 +23,8 @@ const AUTH = {
     SECRET_KEY: makeId() // secret key for token crypt
 };
 
-
-/**
- * only use for dev
- * DELETE ALL THIS ON FINAL PROD
- * @type {{PORT_ANGULAR: number, HOST_EXPRESS: (*|*)}}
- */
-const AUTHDEV = {
-    PORT_EXPRESS : 8080, // use for listen this port on backend
-    HOST_ANGULAR : 'localhost:4200', // use for multi-cross origin request with client
-    SECRET_KEY: makeId() // secret key for token crypt
-};
-
-
 /**
  * export AUTH attribute
- * -----------------------------------------------------------------
- * for developpement. Comment and uncomment the line of ip of you need
- * DELETE 'exports.auth = AUTHDEV;' ON FINAL PROD
  */
 exports.auth = AUTH;
-// exports.auth = AUTHDEV;
+
