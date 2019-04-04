c94f107d62e393476192065c584ba31c07a3a08f
elodie balaia <elodie.balaia@uha.fr>
2019-04-04 17:54:13 +0200

    ajout diagramme coté userspace

--- client/src/app/admin/hebdo/hebdo.component.ts
+++ client/src/app/admin/hebdo/hebdo.component.ts
@@ -41,32 +41,28 @@ export class HebdoComponent implements OnInit {
     this.PieChart = new Chart('pieChart', {
       type: 'pie',
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Présence", "Maladie", "Stage", "Autre"],
         datasets: [{
           label: '# of Votes',
-          data: [9,7 , 3, 5, 2, 10],
+          data: [9,7 , 3, 5],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
             'rgba(255, 206, 86, 0.2)',
             'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
           ],
           borderColor: [
             'rgba(255,99,132,1)',
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
             'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
           ],
           borderWidth: 1
         }]
       },
       options: {
         title:{
-          text:"Bar Chart",
+          text:"Répartitions des heures",
           display:true
         },
         scales: {
