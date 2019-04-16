9dadab096011b79c033e8bca24224bd0c2ce5fb7
thomas.tosch <thomas.tosch@uha.fr>
2019-04-16 15:26:46 +0200

    Further with tests

--- test.js
+++ test.js
@@ -2,28 +2,36 @@ const http = require('http');
 const assert = require('assert');
 
 describe('UUID Route Test', function () {
-    before(function () {
-        const uuid_value = '55555555'
-    });
+
+    const endpoint = 'http://127.0.0.1:8080/uuid';
+
+    const options = {
+        hostname: '127.0.0.1',
+        port: 8080,
+        path: '/uuid',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const uuid_value = {
+        uuid: '55555555'
+    };
+
     describe('/uuid', function () {
         it('should be Badgeuse intelligente : UUID', function (done) {
-            http.get('http://127.0.0.1:8080/uuid', function (response) {
+            http.get(endpoint, function (response) {
                 assert.equal(response.statusCode, 200);
-
                 var body = '';
-                response.on('data', function(d) {
+                response.on('data', function (d) {
                     body += d;
                 });
-                response.on('end', function() {
-                    // Let's wait until we read the response, and then assert the body
-                    // is 'Hello, Mocha!'.
+                response.on('end', function () {
                     assert.equal(body, 'Badgeuse intelligente : UUID');
                     done();
                 });
             })
-        })
-        it('should ', function () {
-            
         });
     });
 });
