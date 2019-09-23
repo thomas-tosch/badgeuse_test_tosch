57ff7dbc2226e2bdbf5d44e131eab754607f3bdb
thomas.tosch <thomas.tosch@uha.fr>
2019-09-23 14:19:41 +0200

    Added user tests, started upload test. Added David.

---
+++ test/upload.js
@@ -0,0 +1,32 @@
+const request = require('supertest')('http://127.0.0.1:8080');
+const expect = require("chai").expect;
+
+describe('Upload Route Test', function () {
+    describe('GET', function () {
+        it('should be Badgeuse intelligente : Upload', async () => {
+            const res = await request.get('/upload');
+            expect(res.status).to.equal(200);
+            expect(res.text).to.equal('Badgeuse intelligente : Upload')
+        });
+    });
+    describe('POST', function () {
+        it('should display a 500 error when no data is sent', async () => {
+            const res = await request
+                .post('/upload')
+                .send({});
+            expect(res.status).to.equal(500);
+        });
+        it('should return an error if the file type is incorrect', async () => {
+            const res = await request
+                .post('/upload')
+                .attach('justificatif', __dirname + '/wrong_file.txt');
+            expect(res.text).to.equal("error");
+        });
+        it('should return an error if the file type is incorrect', async () => {
+            const res = await request
+                .post('/upload')
+                .attach('justificatif', __dirname + '/wrong_file.txt');
+            expect(res.text).to.equal("error");
+        });
+    });
+});
--- test/user.js
+++ test/user.js
@@ -28,5 +28,73 @@ describe('User Route Test', function () {
             expect(res.body).to.have.property("message")
                 .to.equal("Vous n'avez rien à faire ici ! USER");
         });
+        it('should display false when cheking for wrong token on checkToken', async () => {
+            const res = await request
+                .post('/user')
+                .send({
+                    "token": token,
+                    "action": "checkToken"
+                });
+            expect(res.body).to.have.property("errorToken")
+                .to.equal(false);
+        });
+        it('should return the user ID when the username is passed on getIdUser', async () => {
+            const res = await request
+                .post('/user')
+                .send({
+                    "token": token,
+                    "action": "getIdUser",
+                    "userName": "bourgeois florent"
+                });
+            expect(res.body).to.have.property("user")
+                .to.equal(1);
+        });
+        it('should contain a user object on getDataUser', async () => {
+            const res = await request
+                .post('/user')
+                .send({
+                    "token": token,
+                    "action": "getDataUser",
+                    "id_user": 1
+                });
+            expect(res.body).to.have.property("user")
+                .to.be.an('object').to.not.be.null;
+        });
+        it('should display a confirmation on update on updateGroup', async () => {
+            const res = await request
+                .post('/user')
+                .send({
+                    "token": token,
+                    "action": "updateGroup",
+                    "id_user": 1,
+                    "id_group": 4
+                });
+            expect(res.body).to.have.property("message").to.equal("L'information a bien été mise à jour.")
+        });
+        it('should provide pieChartData and Reason given a start to end date on getPieChart', async () => {
+            const res = await request
+                .post('/user')
+                .send({
+                    "token": token,
+                    "action": "getPieChart",
+                    "id_user": 1,
+                    "startDate": "",
+                    "endDate": ""
+                });
+            expect(res.body).to.have.property('pieData');
+            expect(res.body).to.have.property('pieReason');
+        });
+        it('should provide pieChartData and Reason given a start to end date on getPieChartAdmin', async () => {
+            const res = await request
+                .post('/user')
+                .send({
+                    "token": token,
+                    "action": "getPieChartAdmin",
+                    "startDate": "",
+                    "endDate": ""
+                });
+            expect(res.body).to.have.property('PieData');
+            expect(res.body).to.have.property('PieReason');
+        });
     });
 });
