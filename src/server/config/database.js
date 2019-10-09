ce13f1c4beb7399331e1a3f6a41e1f886b856775
thomas.tosch <thomas.tosch@uha.fr>
2019-10-09 09:12:06 +0200

    Changed deployment

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 
 db = mysql.createPool(
     {
-            host     : 'mariadb',
+            host     : process.env.MYSQL_HOST,
             port     : process.env.MYSQL_PORT,
             user     : process.env.MYSQL_USER,
             password : process.env.MYSQL_PASSWORD,
