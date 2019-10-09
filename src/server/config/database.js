fcb6ce240827bcb3b73ece6d9b8c64f6b3ff9f3e
thomas.tosch <thomas.tosch@uha.fr>
2019-10-09 11:36:31 +0200

    Trying to fix db

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -4,8 +4,8 @@ db = mysql.createPool(
     {
             host     : process.env.MYSQL_HOST,
             port     : process.env.MYSQL_PORT,
-            user     : process.env.MYSQL_USER,
-            password : process.env.MYSQL_PASSWORD,
+            user     : process.env.MYSQL_USER || "uhaSQL",
+            password : process.env.MYSQL_PASSWORD || "uha",
             database : process.env.MYSQL_DATABASE,
             multipleStatements: true // ATTENTION CETTE INFORMATIONS PEUT-ETRE A LORIGINE DE GROS PROBLEMES
     });
