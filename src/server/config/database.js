2edf0b433cc669c8de2679153a5c12025a526eb0
thomas.tosch <thomas.tosch@uha.fr>
2019-10-21 12:24:47 +0200

    Fixed env

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -2,10 +2,10 @@ let mysql = require('mysql');
 
 db = mysql.createPool(
     {
-            host     : process.env.MYSQL_HOST,
+            host     : process.env.MYSQL_HOST || "127.0.0.1",
             port     : process.env.MYSQL_PORT || 3306,
             user     : process.env.MYSQL_USER || "uhaSQL",
             password : process.env.MYSQL_PASSWORD || "uha",
-            database : process.env.MYSQL_DATABASE || "127.0.0.1",
+            database : process.env.MYSQL_DATABASE || "badgeuse",
             multipleStatements: true // ATTENTION CETTE INFORMATIONS PEUT-ETRE A LORIGINE DE GROS PROBLEMES
     });
